5b581d712c8f7be3b31f4db3aaa3c6977d778313
satvikvuppala <vuppala.satvik@genzsoln.com>
2023-08-16 16:39:51 +0530

    Recorded sessions

--- cypress/e2e/wikipedia.cy.js
+++ cypress/e2e/wikipedia.cy.js
@@ -5,9 +5,11 @@ it('Wikipedia',()=>{
     cy.visit('https://www.wikipedia.org/')
     cy.get('#searchInput').type('Lufthansa')
     cy.get('[href="https://en.wikipedia.org/wiki/Lufthansa"] > .suggestion-text > .suggestion-title > .suggestion-highlight').click()
+    cy.wait(2000)
     cy.get('#Destinations').scrollIntoView()
+    cy.wait(2000)
     cy.get(':nth-child(135) > a').click()
-
+    cy.wait(5000)
 
 
 })
